d00e26718dd0a357327e994471794a29d7519226
Alex Chika <mcluckey1@gmail.com>
2022-05-24 19:28:30 +0100

    app complteted

--- src/App.js
+++ src/App.js
@@ -27,23 +27,3 @@ function App() {
 }
 
 export default App;
-// // "homepage": "https://alexchika.github.io/kodecamptaskfive",
-// React_Router tutorial
-// import { Outlet, Link } from "react-router-dom";
-// export default function App() {
-//   return (
-//     <div>
-//       <h1>Bookkeeper</h1>
-//       <nav
-//         style={{
-//           borderBottom: "solid 1px",
-//           paddingBottom: "1rem",
-//         }}
-//       >
-//         <Link to="/invoices">Invoices</Link> |{" "}
-//         <Link to="/expenses">Expenses</Link>
-//       </nav>
-//       <Outlet />
-//     </div>
-//   );
-// }
